0743f37306a8b2a48a56214c21593bc3cf9e292e
Anurag Pathak <101109228+pathakanu@users.noreply.github.com>
2024-01-22 23:11:55 +0530

    rebot update

--- rebot_gemini_2.py
+++ rebot_gemini_2.py
@@ -35,7 +35,7 @@ def summarize_document(pdf_content,prompt):
     return response.text
 
 def get_gemini_list(pdf_content,prompt):
-    model=genai.GenerativeModel('gemini-pro')
+    model=genai.GenerativeModel('gemini-pro-vision')
     response=model.generate_content([pdf_content,prompt])
     # print(response.text)
     return response.text
@@ -107,46 +107,23 @@ st.set_page_config(page_title="ATS Resume EXpert")
 st.header("TFT Resume Screening Bot")
 # input_text=st.text_area("Job Description: ",key="input")
 input_text = "The position of Python Developer (GraphQL) requires a minimum of 6 years of experience and expertise in Python, Django, and GraphQL. The role involves designing and building GraphQL schema, developing APIs, troubleshooting issues, and writing clean and reusable code. The candidate should have excellent communication skills, problem-solving abilities, and attention to detail. Think Future Technologies (TFT) is a technology services company that partners with clients to provide optimal solutions and delivery for their business needs."
-prompt = """You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
-the job description. Also mention keywords missing from resume in Job description. Below is the provided sample format in which you need to give output. 
-Example: {
-"Candidate name": "Rahul Robin",
-"Match%": "58",
-"Missing Keywords": "Skills not present in CV but present in Job Description"
-}
-Make sure you don't replicate the example, based on the given resume provide the output"""
+
 
 summarize_prompt = """
 Please summarize the given document mentioning total experience ,skillsets and qualification, don't include "About Us" Section which contains company details. Only mention the required data in JD, summarize within 100 words"""
 
 summarize_resume_prompt = """
-Please summarize the given resume mentioning total experience ,skillsets and qualification. Figure out the domain of the candidates, based on domain include the tech stacks from the projects section under roles and responsibilities of resume"""
+Please summarize the given resume mentioning total working experience ,skillsets and qualification. Exclude the project section of the resume, mention only skills which present in roles and responsibilities"""
 
 uploaded_file=st.file_uploader("Upload your Job Description(PDF)...",type=["pdf"])
-
 if uploaded_file is not None:
-    filename = uploaded_file.name
-    file_path = os.path.join("requirement/",filename)
-    pdf_document = document_load(file_path)
-    summarize_text = summarize_document(pdf_document,summarize_prompt)
-    st.write(summarize_text)
-    st.write("PDF Uploaded Successfully")
-
-# if document is not None:
-#     directory_contents = os.listdir("resumes/")
-#     for item in directory_contents:
-#             file_path = os.path.join("/home/anurag/Documents/langchain/ReBot/resumes",item)
-#             pdf_content=document_load(file_path)
-#             # print(pdf_content)
-#             response=get_gemini_response(prompt,pdf_content,input_text)
-#             try:
-#                 json_object = json.loads(response.text)
-#                 print(json_object)
-#                 document.append(json_object)
-
-#             except json.decoder.JSONDecodeError as e:
-#                 print(f"Error decoding JSON: {e}")
+    with st.spinner('Uploading...'):
+        filename = uploaded_file.name
+        file_path = os.path.join("requirement/",filename)
+        pdf_document = document_load(file_path)
+        summarize_text = summarize_document(pdf_document,summarize_prompt)
+        # st.write(summarize_text)
+        st.write("PDF Uploaded Successfully")
 
 # submit1 = st.button("Tell Me About the Resume")
 
@@ -154,7 +131,24 @@ if uploaded_file is not None:
 
 # submit3 = st.button("Percentage match")
 # submit4 = st.button("Tell me name")
+stack = st.selectbox("Choose JD Domain",("Data Science","Blockchain","Web Development","DevOps","Backend Developer","UI/UX"))
 submit = st.button("Submit")
+
+
+prompt = f"""You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of {stack}, 
+your task is to evaluate the resume against the provided job description. Give me the percentage of match based on the years of experience and tech stacks from the given job description. Also, mention keywords missing from the resume compared to the job description. Below is the provided sample format in which you need to give output. 
+
+Example: {{
+    "Candidate Name": "Rahul Robin",
+    "Total Experience": "6+ years",
+    "Domain Expertise": "Banking and Finance",
+    "Match%": "58",
+    "Known Skills": "MySQL, React.js"
+}}
+
+Please read the given summarized CV thoroughly
+Make sure you don't replicate the example. Based on the given resume, provide the output.
+"""
 
 input_prompt1 = """
  You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
@@ -178,8 +172,8 @@ You are an skilled ATS (Applicant Tracking System) scanner with a deep understan
 your task is to evaluate the resume against the provided job description. Tell me the name of the candidate
 """
 
-input_prompt5 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+input_prompt5 = f"""
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of {stack}, 
 The provided document is dictornary of candidates with %match and missing skills, your task is to evaluate the resume based on match%. Tell me the top 5 candidates for screening based on the list
 """
 
@@ -211,28 +205,40 @@ The provided document is dictornary of candidates with %match and missing skills
 #         st.write("Please upload the resume")
 
 if submit:
-    if document is not None:
-        directory_contents = os.listdir("resumes/")
-        for item in directory_contents:
-            file_path = os.path.join("/home/anurag/Documents/langchain/ReBot/resumes",item)
-            pdf_content=document_load(file_path)
-            pdf_summarized = summarize_document(pdf_content,summarize_resume_prompt)
+    with st.spinner('Searching...'):
+        if document is not None:
+            directory_contents = os.listdir("resumes/")
+            summarize_cv=[]
+            for item in directory_contents:
+                file_path = os.path.join("/home/anurag/Documents/langchain/ReBot/resumes",item)
+                pdf_content=document_load(file_path)
+                pdf_summarized = summarize_document(pdf_content,summarize_resume_prompt)
+                summarize_cv.append(pdf_summarized)
+                
             # print(pdf_content)
             # print(pdf_summarized)
-            st.write(pdf_summarized)
-            response=get_gemini_response(summarize_text,pdf_summarized,prompt)
-            try:
-                json_object = json.loads(response.text)
-                print(json_object)
-                st.write(json_object)
-                document.append(json_object)
+                # st.write(pdf_summarized)
+                response=get_gemini_response(summarize_text,pdf_summarized,prompt)
+                try:
+                    json_object = json.loads(response.text)
+                    print(json_object)
+                    st.write(json_object)
+                    document.append(json_object)
 
-            except json.decoder.JSONDecodeError as e:
-                print(f"Error decoding JSON: {e}")
+                except json.decoder.JSONDecodeError as e:
+                    print(f"Error decoding JSON: {e}")
         # pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_list(document,input_prompt5)
-        st.subheader("The Repsonse is")
-        st.write(response.parts)
-        st.write(document)
-    else:
-        st.write("Please upload the resume")
+            for summary in summarize_cv:
+                with open("summarized_cv.txt", "a") as file:
+                    file.write(summary)
+                    file.write('\n\n\n\n')
+            sorted_candidates = sorted(document, key=lambda x: int(x['Match%']), reverse=True)
+            top_5_candidates = sorted_candidates[:5]
+            st.subheader("Top 5 Candidate")
+            for candidate in top_5_candidates:
+                st.write(f"Candidate Name: {candidate['Candidate Name']}, Match%: {candidate['Match%']}")
+            # st.subheader("Top 5 Candidate")
+            # st.write(response.text)
+            # st.write(document)
+        else:
+            st.write("Please upload the resume")
